f899398201db9741046b19deba27bc575710e67f
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-12-03 18:09:33 -0330

    d3: get rid of string conversion

--- 2025/d3/solution.exs
+++ 2025/d3/solution.exs
@@ -7,16 +7,16 @@ defmodule Aoc do
   def part1(input) do
     parse_input(input)
     |> Enum.map(&calc_max_joltage(&1, 2))
-    |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
 
   def part2(input) do
     parse_input(input)
     |> Enum.map(&calc_max_joltage(&1, 12))
-    |> Enum.map(&String.to_integer/1)
     |> Enum.sum()
   end
+
+  defp calc_max_joltage(_bank, 0), do: 0
 
   defp calc_max_joltage(bank, max_batteries) do
     search_window_size = length(bank) - max_batteries + 1
@@ -28,10 +28,10 @@ defmodule Aoc do
 
     remaining_bank = Enum.drop(bank, max_index + 1)
 
-    Integer.to_string(max_val) <> calc_max_joltage(remaining_bank, max_batteries - 1)
-  end
+    current_tens_place = max_val * 10 ** (max_batteries - 1)
 
-  defp calc_max_joltage(_bank, 0), do: ""
+    current_tens_place + calc_max_joltage(remaining_bank, max_batteries - 1)
+  end
 end
 
 sample = File.read!("./sample.txt")
