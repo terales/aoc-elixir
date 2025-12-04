0119dd836a689f1e30c46ccbaf797d4c065d3df4
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-12-04 07:00:24 -0330

    d4: refactor with new learnings about for loops

--- 2025/d4/solution.exs
+++ 2025/d4/solution.exs
@@ -1,53 +1,39 @@
 defmodule Aoc do
-  defp parse_input(input) do
-    Grid.build_grid(input)
-    |> Grid.take_cells(["@"])
-  end
-
   def part1(input) do
-    parse_input(input)
-    |> accessible_rols_positions()
-    |> Enum.count()
+    Grid.build_grid(input, ["@"])
+    |> accessible_rolls_positions()
+    |> length()
   end
 
   def part2(input) do
-    parse_input(input)
-    |> drop_accessible_rolls_recursive()
+    grid = Grid.build_grid(input, ["@"])
+    cleaned_grid = drop_accessible_rolls_recursive(grid)
+    map_size(grid) - map_size(cleaned_grid)
   end
 
-  def accessible_rols_positions(grid) do
-    grid
-    |> Map.keys()
-    |> Enum.filter(fn position ->
-      Enum.count(Grid.neigbouring_positions(position), &Map.has_key?(grid, &1)) < 4
-    end)
+  def accessible_rolls_positions(grid) do
+    for {position, _} <- grid,
+      Enum.count(Grid.neigbouring_positions(position), &Map.has_key?(grid, &1)) < 4,
+      do: position
   end
 
-  def drop_accessible_rolls_recursive(grid, rolls_removed \\ 0) do
-    rolls_to_remove = accessible_rols_positions(grid)
-
-    case rolls_to_remove do
-      [] -> rolls_removed
-      _ -> grid
+  def drop_accessible_rolls_recursive(grid) do
+    case accessible_rolls_positions(grid) do
+      [] -> grid
+      rolls_to_remove -> grid
         |> Map.drop(rolls_to_remove)
-        |> drop_accessible_rolls_recursive(rolls_removed + length(rolls_to_remove))
+        |> drop_accessible_rolls_recursive()
     end
   end
 end
 
 defmodule Grid do
-  def build_grid(input) do
-    lines = String.split(input, "\n", trim: true)
-
-    for {line, row} <- Enum.with_index(lines),
-        {char, col} <- Enum.with_index(String.to_charlist(line)),
-        into: %{} do
-      {{col, row}, <<char>>}
-    end
-  end
-
-  def take_cells(grid, values_to_keep) do
-    Map.filter(grid, fn {_, value} -> value in values_to_keep end)
+  def build_grid(input, cells_to_keep) do
+    for {line, row} <- Enum.with_index(String.split(input, "\n", trim: true)),
+        {cell, col} <- Enum.with_index(String.graphemes(line)),
+        cell in cells_to_keep,
+        into: %{},
+        do: {{col, row}, cell}
   end
 
   def neigbouring_positions({col, row}) do
@@ -60,17 +46,12 @@ defmodule Grid do
 end
 
 sample = File.read!("./sample.txt")
-IO.puts "Sample (part 1)"
-IO.inspect(Aoc.part1(sample))
-
 input = File.read!("./input.txt")
-IO.puts "Part 1"
-IO.puts Aoc.part1(input)
 
-sample = File.read!("./sample.txt")
-IO.puts "Sample (part 2)"
-IO.inspect(Aoc.part2(sample))
+IO.puts("--- Part 1 ---")
+sample |> Aoc.part1() |> IO.inspect(label: "Sample")
+input  |> Aoc.part1() |> IO.inspect(label: "Input")
 
-input = File.read!("./input.txt")
-IO.puts "Part 2"
-IO.puts Aoc.part2(input)
+IO.puts("\n--- Part 2 ---")
+sample |> Aoc.part2() |> IO.inspect(label: "Sample")
+input  |> Aoc.part2() |> IO.inspect(label: "Input")
